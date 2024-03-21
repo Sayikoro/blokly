d9199df15aaff7e4241772fef178a2f3935c0f2c
Sayikoro <60092876+Sayikoro@users.noreply.github.com>
2024-03-21 18:12:45 +1100

    Add files via upload

---
+++ en.js
@@ -0,0 +1,328 @@
+Msg = {
+  buttons: {
+    save: {
+      desc: "💾 Save Blocks:",
+      button: "Save",
+    },
+    load: {
+      desc: "📂 Load:",
+    },
+    export: {
+      desc: "📄 Export to HTML:",
+      button: "Export",
+    },
+  },
+  categories: {
+    "html": "HTML",
+    "baseframe": "Основные",
+    "textstructure": "Текст(структура)",
+    "markup": "Текст(разметка)",
+    "style": "Стили",
+    "enumerations": "Списки",
+    "tables": "Таблицы",
+    "forms": "Формы",
+    "scripts": "JS",
+    "scripting": "Скриптинг",
+    "logic": "Logic",
+    "loops": "Loops",
+    "math": "Math",
+    "text": "Text",
+    "lists": "Lists",
+    "colour": "Colour",
+    "variables": "Variables",
+    "functions": "Functions",
+  },
+  blocks: {
+    "baseframe": {
+      "message0": "document %1 header %2 %3 content %4 %5",
+    },
+    "html": {
+      "message0": "document %1 %2",
+    },
+    "body": {
+      "message0": "content %1 %2",
+    },
+    "head": {
+      "message0": "header %1 %2",
+    },
+    "title": {
+      "message0": "title %1",
+    },
+    "global_style": {
+      "message0": "selector %1 style %2",
+    },
+    "p": {
+      "message0": "paragraph %1",
+    },
+    "plaintext": {
+      "message0": "Text %1",
+    },
+    "text_print": {
+      "message0": "Text %1",
+    },
+    "text": {
+      "message0": "Text %1",
+    },
+    "division": {
+      "message0": "division %1 %2",
+    },
+    "style": {
+      "message0": "style =  %1 %2",
+      "tooltip": "",
+      "helpUrl": ""
+    },
+    "tag_selector": {
+      "message0": "tag %1",
+      "tooltip": "",
+      "helpUrl": ""
+    },
+    "class_selector": {
+      "message0": "class %1",
+      "tooltip": "",
+      "helpUrl": ""
+    },
+    "color": {
+      "message0": "text colour :  %1",
+      "tooltip": "",
+      "helpUrl": ""
+    },
+    "bgcolour": {
+      "message0": "background colour :  %1",
+      "helpUrl": ""
+    },
+    "margin": {
+      "message0": "margin :  %1",
+      "helpUrl": ""
+    },
+    "padding": {
+      "message0": "padding :  %1",
+      "helpUrl": ""
+    },
+    "genericstyle": {
+      "message0": "%1 : %2",
+      "tooltip": "",
+      "helpUrl": "",
+      "args0": [
+        {"text": "property"},
+        {"text": "value"}
+      ],
+    },
+    "generictag": {
+      "message0": "< %1 > %2 %3",
+      "tooltip": "",
+      "helpUrl": "http://www.w3schools.com/tags/tag_html.asp",
+      "args0": [{"text": "tag"},],
+    },
+    "more_attributes": {
+      "message0": "%1 %2 %3",
+      "tooltip": "",
+      "helpUrl": ""
+    },
+    "genericattribute": {
+      "message0": "%1  =  %2",
+      "tooltip": "",
+      "helpUrl": "",
+      "args0": [
+        {"text": "attribute"},
+        {"text": "value"}
+      ],
+    },
+    "link": {
+      "message0": "link to %1 %2 %3",
+      "tooltip": "",
+      "helpUrl": "http://www.w3schools.com/tags/tag_html.asp",
+      "args0": [{"text": "target"}],
+    },
+    "span": {
+      "message0": "span %1 %2",
+      "tooltip": "",
+      "helpUrl": "http://www.w3schools.com/tags/tag_html.asp"
+    },
+    "image": {
+      "message0": "image %1 or %2",
+      "tooltip": "",
+      "helpUrl": "http://www.w3schools.com/tags/tag_html.asp",
+      "args0": [
+        {"text": "URL"},
+        {"text": "alternative text"}
+      ],
+    },
+    "emphasise": {
+      "message0": "emphasise %1 %2",
+      "tooltip": "",
+      "helpUrl": "http://www.w3schools.com/tags/tag_html.asp"
+    },
+    "strong": {
+      "message0": "important %1 %2",
+      "tooltip": "",
+      "helpUrl": "http://www.w3schools.com/tags/tag_html.asp"
+    },
+    "headline": {
+      "message0": "headline %1 %2 %3",
+      "tooltip": "",
+      "helpUrl": "http://www.w3schools.com/tags/tag_html.asp",
+      "args0": [{"options": [
+        ["level 1","h1"],
+        ["level 2","h2"],
+        ["level 3","h3"],
+        ["level 4","h4"],
+        ["level 5","h5"],
+        ["level 6","h6"]
+      ]}],
+    },
+    "linebreak": {
+      "message0": "line break",
+      "tooltip": "",
+      "helpUrl": "http://www.w3schools.com/tags/tag_html.asp"
+    },
+    "horizontalbreak": {
+      "message0": "topic break",
+      "tooltip": "",
+      "helpUrl": "http://www.w3schools.com/tags/tag_html.asp"
+    },
+    "unorderedlist": {
+      "message0": "unordered list %1 %2",
+      "tooltip": "",
+      "helpUrl": "http://www.w3schools.com/tags/tag_html.asp"
+    },
+    "orderedlist": {
+      "message0": "ordered list %1 %2",
+      "tooltip": "",
+      "helpUrl": "http://www.w3schools.com/tags/tag_html.asp"
+    },
+    "listelement": {
+      "message0": "list item %1 %2",
+      "tooltip": "",
+      "helpUrl": "http://www.w3schools.com/tags/tag_html.asp"
+    },
+    "inserted": {
+      "message0": "inserted at %1 %2",
+      "tooltip": "",
+      "helpUrl": "https://wiki.selfhtml.org/wiki/Referenz:HTML/ins"
+    },
+
+    "deleted": {
+      "message0": "deleted %1 %2",
+      "tooltip": "",
+      "helpUrl": "https://wiki.selfhtml.org/wiki/Referenz:HTML/del"
+    },
+
+    "super": {
+      "message0": "super %1 %2",
+      "tooltip": "",
+      "helpUrl": "https://wiki.selfhtml.org/wiki/Referenz:HTML/sup"
+    },
+
+    "sub": {
+      "message0": "sub %1 %2",
+      "tooltip": "",
+      "helpUrl": "https://wiki.selfhtml.org/wiki/Referenz:HTML/sub"
+    },
+
+    "code": {
+      "message0": "code %1 %2",
+      "tooltip": "",
+      "helpUrl": "https://wiki.selfhtml.org/wiki/Referenz:HTML/code"
+    },
+
+    "quote": {
+      "message0": "quote %1 %2",
+      "tooltip": "",
+      "helpUrl": "https://wiki.selfhtml.org/wiki/Referenz:HTML/q"
+    },
+
+    "blockquote": {
+      "message0": "blockquote %1 %2",
+      "tooltip": "",
+      "helpUrl": "https://wiki.selfhtml.org/wiki/Referenz:HTML/blockquote"
+    },
+
+    "sample": {
+      "message0": "sample %1 %2",
+      "tooltip": "",
+      "helpUrl": "https://wiki.selfhtml.org/wiki/Referenz:HTML/samp"
+    },
+
+    "keyboard": {
+      "message0": "keyboard %1 %2",
+      "tooltip": "",
+      "helpUrl": "https://wiki.selfhtml.org/wiki/Referenz:HTML/kbd"
+    },
+
+    "variable": {
+      "message0": "variable %1 %2",
+      "tooltip": "",
+      "helpUrl": "https://wiki.selfhtml.org/wiki/Referenz:HTML/var"
+    },
+
+    "form": {
+      "message0": "form %1 %2",
+      "tooltip": "",
+      "helpUrl": "https://wiki.selfhtml.org/wiki/Referenz:HTML/form"
+    },
+
+    "table": {
+      "message0": "table %1 %2",
+      "tooltip": "",
+      "helpUrl": "https://wiki.selfhtml.org/wiki/Referenz:HTML/table"
+    },
+
+    "tablerow": {
+      "message0": "row %1 %2",
+      "tooltip": "",
+      "helpUrl": "https://wiki.selfhtml.org/wiki/Referenz:HTML/tr"
+    },
+
+    "tablecell": {
+      "message0": "cell %1 %2",
+      "tooltip": "",
+      "helpUrl": "https://wiki.selfhtml.org/wiki/Referenz:HTML/td"
+    },
+
+    "input_text": {
+      "message0": "input text %1",
+      "tooltip": "",
+      "helpUrl": "https://wiki.selfhtml.org/wiki/Referenz:HTML/input"
+    },
+
+    "button": {
+      "message0": "button %1 %2",
+      "tooltip": "",
+      "helpUrl": "https://wiki.selfhtml.org/wiki/Referenz:HTML/button"
+    },
+
+    "input": {
+      "message0": "%1 input %2 %3",
+      "args0": [{ "options": [
+        ["Text", "text"],
+        ["E-Mail", "email"],
+        ["Number", "number"],
+        ["Password", "password"],
+        ["Checkbox", "checkbox"],
+        ["Radio", "radio"],
+        ["Button", "button"],
+        ["Color", "color"],
+        ["Date", "date"],
+        ["DateTime-Local", "datetime-local"],
+        ["File", "file"],
+        ["Hidden", "hidden"],
+        ["Image", "image"],
+        ["Month", "month"],
+        ["Range", "range"],
+        ["Reset", "reset"],
+        ["Search", "search"],
+        ["Submit", "submit"],
+        ["Telephone Number", "tel"],
+        ["Time", "time"],
+        ["URK", "url"],
+        ["Week", "week"]]
+      }],
+      "tooltip": "",
+      "helpUrl": "https://wiki.selfhtml.org/wiki/Referenz:HTML/input"
+    }
+  }
+}
+
+
+
+
